feba9281589edccdf2435032e03af99af384e1f2
MahmutYnc <yncmahmut@gmail.com>
2023-12-02 01:09:12 +0300

    Refactor Day01.kt and increase its functionality
    The file Day01.kt was renamed and moved to a new directory, part of the structure, to unify the project's organization. Also, new functionality was added. The solution for 'part1' and 'part2' methods were rewritten to calculate calibration values for each line now. Two helper functions 'getCalibrationValueForEachLine' and 'convertLetterDigitToInt' were introduced to improve readability and maintainability of the code. This refactoring allows handling both number and digit-letter mixed inputs.

---
+++ src/day01/Day01.kt
@@ -0,0 +1,111 @@
+package day01
+
+import readInput
+
+fun main() {
+
+    fun getCalibrationValueForEachLine(input: String): Int? {
+        // Find first digit
+        val firstDigit = input.find { it.isDigit() }?.toString() ?: return null
+
+        // Find last digit by checking the string from end
+        val lastDigit = input.reversed().find { it.isDigit() }?.toString() ?: return null
+
+        // Combine first and last digit to get the calibration value
+        return "$firstDigit$lastDigit".toInt()
+    }
+
+    fun part1(input: List<String>): Int {
+        return input
+            .mapNotNull { getCalibrationValueForEachLine(it) }
+            .sum()
+    }
+
+    fun convertLetterDigitToInt(input: String): String {
+        val digitWords = mapOf(
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9"
+        )
+
+        if (input.all { it.isDigit() }) {
+            return input
+        }
+
+        val inputChunks = mutableListOf<String>()
+        var chunk = ""
+
+        // Break the input apart into chunks at each number
+        for (i in input.indices) {
+            if (input[i].isLetter()) {
+                chunk += input[i]
+                digitWords.forEach { (key, value) ->
+                    chunk = chunk.replace(key, value)
+                    if (key == chunk) {
+                        inputChunks += chunk.replace(key, value)
+                        chunk = input[i].toString()
+                    }
+                }
+            } else if (input[i].isDigit()) {
+                inputChunks += chunk
+                inputChunks += input[i].toString()
+                chunk = ""
+            } else {
+                inputChunks += chunk
+                chunk = input[i].toString()
+            }
+        }
+        inputChunks += chunk // Add the last chunk
+
+        val chunkResult = inputChunks.joinToString(separator = "")
+
+        val firstDigit = chunkResult.find { it.isDigit() }
+
+        chunk = ""
+        val inputChunks2 = mutableListOf<String>()
+        val reversedInput = input.reversed()
+        // Break the input apart into chunks at each number
+        for (i in reversedInput.indices) {
+            if (reversedInput[i].isLetter()) {
+                chunk += reversedInput[i]
+                digitWords.forEach { (key, value) ->
+                    chunk = chunk.replace(key.reversed(), value)
+                    if (key == chunk) {
+                        inputChunks2 += chunk.replace(key.reversed(), value)
+                        chunk = reversedInput[i].toString()
+                    }
+                }
+            } else if (reversedInput[i].isDigit()) {
+                inputChunks2 += chunk
+                inputChunks2 += reversedInput[i].toString()
+                chunk = ""
+            } else {
+                inputChunks2 += chunk
+                chunk = reversedInput[i].toString()
+            }
+        }
+        inputChunks2 += chunk // Add the last chunk
+
+        val chunkResult2 = inputChunks2.joinToString(separator = "")
+
+        val lastDigit = chunkResult2.find { it.isDigit() }
+        return "${firstDigit.toString()}${lastDigit.toString()}"
+    }
+
+    fun part2(input: List<String>): Int {
+        return input
+            .mapNotNull { getCalibrationValueForEachLine(convertLetterDigitToInt(it)) }
+            .sum()
+    }
+
+    val input = readInput("./day01/Day01_input")
+
+    println(part1(input))
+    println(part2(input))
+}
